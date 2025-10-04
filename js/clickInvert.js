013c1a177f7775b00b9035f7628c8fa7884ff88a
Hamburami <harrisonpena1@gmail.com>
2025-10-04 16:37:34 -0600

    edits

--- js/clickInvert.js
+++ js/clickInvert.js
@@ -2,16 +2,20 @@ var root = document.querySelector(':root');
 var darkMode = false;
 const fly = document.getElementById('fly');
 
-addEventListener("click", (event) => { 
-    if (darkMode) {
-        document.documentElement.style.cssText = "--primary-text: rgb(122, 34, 28); --time-color: rgb(255,255,255)";
-        fly.style.filter = 'invert(0)';
-        darkMode = false;
-    } else {
-        document.documentElement.style.cssText =  "--primary-text: rgb(20, 202, 202); --time-color: rgb(0,0,0)";
-        //document.body.style.backgroundColor = 'var(--time-color)';
-        fly.style.filter = 'invert(1)';
-        darkMode = true;
-    }
-})
 
+const toggle = () => {
+  if (darkMode) {
+    document.documentElement.style.cssText =
+      "--primary-text: rgb(122, 34, 28); --time-color: rgb(255,255,255)";
+    fly.style.filter = "invert(0)";
+  } else {
+    document.documentElement.style.cssText =
+      "--primary-text: rgb(20, 202, 202); --time-color: rgb(0,0,0)";
+    fly.style.filter = "invert(1)";
+  }
+  darkMode = !darkMode;
+};
+
+
+document.body.addEventListener("click", toggle);
+document.body.addEventListener("touchstart", toggle);
